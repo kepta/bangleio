072cd7b45171588e26637a6e50f316fe3c5afc1f
sahiljain112 <sahiljain112@gmail.com>
2016-07-15 21:39:26 +0530

    Added GetHistory

--- app/network/getData.js
+++ app/network/getData.js
@@ -4,3 +4,8 @@ export function getCurrentPage(page) {
   return fetch(`${FIREBASE_URL}/data/${page}/current.json`)
           .then((resp) => resp.json());
 }
+
+export function getHistoryByPage(page) {
+  return fetch(`${FIREBASE_URL}/history/${page}/current.json`)
+          .then((resp) => resp.json());
+}
